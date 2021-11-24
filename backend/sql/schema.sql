3505bad980e076fd4ec81a72b9348deb9a493cf6
bhbyun1 <bradbyun1@gmail.com>
2021-11-24 14:30:36 -0800

    Mini databases complete

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,4 +1,13 @@
 -- Your database schema goes here --
+-- CREATE TABLE member(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
+-- CREATE TABLE category(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), category jsonb);
+-- CREATE TABLE listing(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),  categoryID text, FOREIGN KEY(categoryID) REFERENCES category(id), memberID text, FOREIGN KEY (memberID) REFERENCES member(id), listings jsonb);
+-- -- Your database schema goes here --
+-- CREATE TABLE member(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
+-- CREATE TABLE category(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), category jsonb);
+-- CREATE TABLE listing(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), categoryID text, memberID text, listings jsonb);
 CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
-CREATE TABLE category(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), category jsonb);
-CREATE TABLE listings(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), listings jsonb);
+CREATE TABLE category(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), parent UUID, names VARCHAR(100));
+-- CREATE TABLE listing(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),  categoryID UUID, memberID UUID, listings jsonb);
+CREATE TABLE listing(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),  categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), listings jsonb);
+-- CREATE TABLE listing(id text UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), categoryID text, memberID text, listings jsonb);
