4d4a60be82c2df3cf79e35f8d475277cac9b3383
Gavin <gavin.poley@gmail.com>
2021-11-23 18:12:52 -0800

    updating WIP sql stuff

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -2,3 +2,5 @@
 INSERT INTO dummy (created) VALUES (current_timestamp);
 
 -- Populate Your Tables Here --
+-- INSERT INTO member (member) VALUES ({'name':'Sarena Hewell','email':'shewell0@imageshack.us','password':'hlF092tzrfBK'});
+-- INSERT INTO member (member) VALUES ({'name':'Erinna Thewys','email':'ethewys1@so-net.ne.jp','password':'KsHP7p1Fio'});
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -3,3 +3,6 @@ DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
+CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
+CREATE TABLE category(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), category jsonb);
+CREATE TABLE listings(id UUID UNIQUE PRIMARY KEY gen_random_uuid(), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), listings jsonb);
