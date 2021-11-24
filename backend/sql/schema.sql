ba6682b791e8a21529e890d9abbad5ccee0cb7ba
mhforema <mhforema@ucsc.edu>
2021-11-23 21:10:45 -0800

    fixing schema bug and deleting dummy db

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,8 +1,4 @@
--- Dummy table --
-DROP TABLE IF EXISTS dummy;
-CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
-
 -- Your database schema goes here --
 CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
 CREATE TABLE category(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), category jsonb);
-CREATE TABLE listings(id UUID UNIQUE PRIMARY KEY gen_random_uuid(), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), listings jsonb);
+CREATE TABLE listings(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), listings jsonb);
