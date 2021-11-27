816fbf8a732eba7863a45b21070c199855c4192c
Gavin <gavin.poley@gmail.com>
2021-11-27 15:08:36 -0800

    added popup for login, doesnt work

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -6,12 +6,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 // import IconButton from '@mui/material/IconButton';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Box from '@mui/material/Box';
 
 /**
  *
  * @return {object}
  */
 function TopBar() {
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   const temp = (
     <form noValidate autoComplete="off">
       <TextField
@@ -23,6 +35,36 @@ function TopBar() {
         label='password'
       >search</TextField>
     </form>
+  );
+  const loginScreen = (
+    <Dialog fullScreen open={open} onClose={handleClose}>
+      <AppBar sx={{position: 'relative'}}>
+        <Toolbar>
+          <Typography variant='h6'>Login Screen</Typography>
+          <IconButton sx={{marginLeft: 'auto'}} onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box sx={{pl: '12%', my: '50%'}}>
+        <Box sx={{pl: '38%'}}>
+          <AccountCircleIcon />
+        </Box>
+        <TextField
+          id='outlined-search'
+          label='username'
+          sx={{my: '2%', width: '300px'}}
+        >USERNAME</TextField>
+        <TextField
+          id='outlined-search'
+          label='password'
+          sx={{my: '3%', width: '300px'}}
+        >PASSWORD</TextField>
+        <Button variant='contained' sx={{width: '300px'}}>
+          Login
+        </Button>
+      </Box>
+    </Dialog>
   );
   const winDims = () => ({
     width: window.innerWidth,
@@ -51,9 +93,12 @@ function TopBar() {
           </div>
           <Button
             style={{color: 'white', marginLeft: 'auto'}}
-            onClick={()=>console.log('login button')}
+            onClick={handleClickOpen}
           >Login
           </Button>
+          <Box>
+            {loginScreen}
+          </Box>
         </Toolbar>
       </AppBar>
       <Toolbar />
