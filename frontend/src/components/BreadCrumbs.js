8f135325a4fa801d550cf9ddc818bf5ec92a98ab
mhforema <mhforema@ucsc.edu>
2021-11-27 19:31:11 -0800

    change it back lol

--- frontend/src/components/BreadCrumbs.js
+++ frontend/src/components/BreadCrumbs.js
@@ -8,41 +8,42 @@ import Button from '@mui/material/Button';
  * @return {object}
  */
 export default function CustomSeparator({setSearch}) {
-  const {currCat, setCategory} = useContext(CategoryContext);
-  const {currSub, setSub} = useContext(CategoryContext);
+const {currCat, setCategory} = useContext(CategoryContext);
+const {currSub, setSub} = useContext(CategoryContext);
   return (
     <div>
-      <Stack spacing={1}>
-        <Breadcrumbs separator="›">
-          <Button color="inherit" display='none'
-            onClick={()=>{
-              setCategory(undefined);
-              setSub(undefined);
-              setSearch('');
-            }}
-          >
-            MARKETPLACE
-          </Button>
-          {currCat !== undefined? <Button
-            underline="hover"
-            key="2"
-            color="inherit"
-            onClick={()=>{
-              setSub(undefined);
-              setSearch('');
-            }}
-          >
-            {currCat}
-          </Button> : ''}
-          {currSub !== undefined? <Button
-            underline="hover"
-            key="2"
-            color="inherit"
-          >
-            {currSub}
-          </Button> : ''}
-        </Breadcrumbs>
-      </Stack>
+    <Stack spacing={1}>
+      <Breadcrumbs separator="›">
+      {currCat !== undefined ?
+        <Button color="inherit" display='none'
+          onClick={()=>{
+            setCategory(undefined);
+            setSub(undefined);
+            setSearch('');
+          }}
+        >
+      MARKETPLACE
+    </Button> : <Button style={{visibility: 'hidden'}}>MARKETPLACe</Button>}
+    {currCat !== undefined? <Button
+      underline="hover"
+      key="2"
+      color="inherit"
+      onClick={()=>{
+        setSub(undefined);
+        setSearch('');
+      }}
+    >
+      {currCat}
+    </Button> : ''}
+    {currSub !== undefined? <Button
+      underline="hover"
+      key="2"
+      color="inherit"
+    >
+      {currSub}
+    </Button> : ''}
+      </Breadcrumbs>
+    </Stack>
     </div>
   );
 }
