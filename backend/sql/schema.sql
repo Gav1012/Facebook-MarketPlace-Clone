2b757c6686fa858263d9d090a50c4c1b1eb26b28
mhforema <mhforema@ucsc.edu>
2021-12-01 11:23:07 -0800

    db update

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -5,5 +5,5 @@ DROP TABLE IF EXISTS listing;
 DROP TABLE IF EXISTS filter;
 CREATE TABLE member(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), member jsonb);
 CREATE TABLE category(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(), parent UUID, names VARCHAR(100));
-CREATE TABLE listing(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),  categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), listings jsonb);
+CREATE TABLE listing(id UUID UNIQUE PRIMARY KEY DEFAULT gen_random_uuid(),  categoryID UUID, FOREIGN KEY(categoryID) REFERENCES category(id), memberID UUID, FOREIGN KEY (memberID) REFERENCES member(id), filterType VARCHAR(100), listings jsonb);
 CREATE TABLE filter(id UUID, parent UUID, names VARCHAR(100), attributes jsonb);
