802f6b5fd7fe180f9cabdfc66f751b49892c97e1
Gavin <gavin.poley@gmail.com>
2021-12-02 12:50:01 -0800

    add listgrid test file

---
+++ frontend/src/components/__tests__/ListGrid.test.js
@@ -0,0 +1,54 @@
+import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {screen, waitFor} from '@testing-library/react';
+import {rest} from 'msw'
+import {setupServer} from 'msw/node'
+import ListGrid from '../ListGrid';
+import CategoryContext from '../CategoryContext';
+
+// const host = 'http://localhost:3010'
+
+const URL = '/v0/listings'
+const server = setupServer(
+  rest.get(URL, (req, res, ctx) => {
+    return res(
+      ctx.json([{id: '1', categoryID: '2', memberID: '3', filterType: 'Men',
+      listings: {"title":"gold necklace","content":"it is made out of gold","summary":"this is a gold necklace from walmart, it has been slightly worn. other than a few scratches here or there it is pretty much brand new.","price":"$582","createDate":"November 23, 2021","images":[{"link":"https://slimages.macysassets.com/is/image/MCY/products/4/optimized/884154_fpx.tif?op_sharpen=1&wid=700&hei=855&fit=fit,1"}]}}]))
+  }),
+)
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const currCat = '';
+const currSub = '';
+const search = '';
+
+/**
+ */
+ test('ListGrid Exists', async () => {
+  render(
+    <CategoryContext.Provider value={{
+      currCat, currSub, search}}>
+      <ListGrid />
+    </CategoryContext.Provider>
+  );
+  await waitFor(() => {
+    expect(screen.getByText('gold necklace'));
+  })
+});
+
+// test('Handle Server Error', async () => {
+//   server.use(
+//     rest.get(URL, (req, res, ctx) => {
+//       return res(ctx.status(404));
+//     }),
+//   )
+//   render(
+//     <CategoryContext.Provider value={{
+//       currCat, currSub, search}}>
+//       <ListGrid />
+//     </CategoryContext.Provider>
+//   );
+// })
