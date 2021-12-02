2543095f23dee064f6c447c7d51886408fc8c13a
Gavin <gavin.poley@gmail.com>
2021-12-02 14:51:37 -0800

    updated app test

--- frontend/src/components/__tests__/App.test.js
+++ frontend/src/components/__tests__/App.test.js
@@ -1,10 +1,21 @@
-import {render} from '@testing-library/react';
+import {render, act} from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import App from '../App';
 
+function setWidth(width) {
+  global.innerWidth = width;
+  act(() => {
+    global.dispatchEvent(new Event('resize'));
+  });
+}
 /**
  */
- test('App Renders', async () => {
+test('App Renders in Desktop form', async () => {
   render(<App />);
 });
+
+test('App renders in Mobile form', async () => {
+  render(<App />);
+  setWidth(550);
+})
