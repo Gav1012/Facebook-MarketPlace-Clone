99ad3b346399d886f19b86be3e22c454cc62e7f8
mhforema <mhforema@ucsc.edu>
2021-12-03 12:09:41 -0800

    top bar test

---
+++ frontend/src/components/__tests__/topbar.test.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import {render, fireEvent} from '@testing-library/react';
+import '@testing-library/jest-dom';
+import {screen, waitFor} from '@testing-library/react';
+import {act} from 'react-dom/test-utils';
+import App from '../App';
+import ListGrid from '../ListGrid';
+import {setupServer} from 'msw/node'
+import {rest} from 'msw'
+import CategoryContext from '../CategoryContext'
+import ListingContext from '../ListingContext'
+import TopBar from '../TopBar';
+
+
+
+const dimensions = {width: 500};
+
+
+test('topbar render', async () => {
+  render(
+    <CategoryContext.Provider value={{ dimensions}}>
+      <TopBar  />
+    </CategoryContext.Provider>
+  );
+  await waitFor(() => {
+      expect(screen.getByText('Login'));
+      fireEvent.click(screen.getByText('Login'));
+    })
+  await new Promise((r) => setTimeout(r, 2000));
+});
+      
